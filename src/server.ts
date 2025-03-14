af03b82d159cd8def0d2afc1bd5374e10c95bd54
Hatsune Miku <l2a1knla@gmail.com>
2025-03-14 15:15:10 +0800

    stash

--- src/server.ts
+++ src/server.ts
@@ -21,6 +21,8 @@ const port = 6308
 expressApp.listen(port, async () => {
   await main()
   await initializeLarkBot()
+
+  expressApp.use(express.json())
   defineRoute(expressApp)
   info(`Server listening at http://localhost:${port}`)
 })
