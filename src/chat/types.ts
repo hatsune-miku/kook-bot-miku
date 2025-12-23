91a377cea26c89543f20fc434b74295ac896b725
Hatsune Miku <l2a1knla@gmail.com>
2025-12-24 03:51:52 +0800

    嘟嘟

--- src/chat/types.ts
+++ src/chat/types.ts
@@ -1,4 +1,5 @@
 export const ChatBotBackends = {
+  gpt51: 'gpt-5.1',
   gpt52: 'gpt-5.2',
   gpt5t: 'gpt-5-thinking',
   deepseekv31volc: 'deepseek-v3-1-250821',
@@ -7,5 +8,5 @@ export const ChatBotBackends = {
 export type ChatBotBackend = (typeof ChatBotBackends)[keyof typeof ChatBotBackends]
 
 export function isReasonerBackend(backend: ChatBotBackend) {
-  return [ChatBotBackends.gpt5t, ChatBotBackends.gpt52].includes(backend as any)
+  return [ChatBotBackends.gpt5t, ChatBotBackends.gpt52, ChatBotBackends.gpt51].includes(backend as any)
 }
